62cd62cc25dfc3d350d81f44e85be4e0a902a710
Albert Khang <albertkhang2017@gmail.com>
2020-07-17 14:28:50 +0700

    draw line and candlestick chart

--- app/src/main/java/com/albertkhang/testcandlestickchart/MainActivity.java
+++ app/src/main/java/com/albertkhang/testcandlestickchart/MainActivity.java
@@ -3,12 +3,49 @@ package com.albertkhang.testcandlestickchart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static ArrayList<CandleEntry> values;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        final CandlesStickChart view = new CandlesStickChart(this);
+//
+//        ViewTreeObserver observer = view.getViewTreeObserver();
+//        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @Override
+//            public void onGlobalLayout() {
+//                Log.d("measure", "height: " + view.getHeight() + ", width: " + view.getWidth());
+//            }
+//        });
+
+        setContentView(view);
+
+        values = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            float val = (float) (Math.random() * 40);
+
+            float high = (float) (Math.random() * 9) + 8f;
+            float low = (float) (Math.random() * 9) + 8f;
+
+            float open = (float) (Math.random() * 6) + 1f;
+            float close = (float) (Math.random() * 6) + 1f;
+
+            boolean even = i % 2 == 0;
+
+            values.add(new CandleEntry(
+                    i, val + high,
+                    val - low,
+                    even ? val + open : val - open,
+                    even ? val - close : val + close
+            ));
+        }
     }
 }
