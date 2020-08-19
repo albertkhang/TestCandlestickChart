9b9f9eff2a05fdbd69731b24f3a7819a8ac9f1bf
Albert Khang <albertkhang2017@gmail.com>
2020-08-19 15:57:54 +0700

    can move grid

---
+++ app/src/main/java/com/albertkhang/testcandlestickchart/Chart.java
@@ -0,0 +1,126 @@
+package com.albertkhang.testcandlestickchart;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.core.view.MotionEventCompat;
+
+public class Chart extends View {
+    private static final String TAG = "Chart";
+
+    public Chart(Context context) {
+        super(context);
+    }
+
+    public Chart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Chart(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Chart(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private Paint paint = new Paint();
+
+    private boolean isDragX = true;
+    private boolean isDragY = false;
+
+    private int mMaxXGrid = 20;
+    private int mMaxYGrid = 20;
+
+    private int mSpacing = 150;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+//        Log.d(TAG, "onDraw");
+
+        paint.setColor(getResources().getColor(R.color.colorPrimaryDark));
+        paint.setStrokeWidth(2);
+
+        for (int i = 0; i < mMaxXGrid; i++) {
+            canvas.drawLine(mPosX + 75 + mSpacing * i, 75, mPosX + 75 + mSpacing * i, 75 + 2000, paint);
+        }
+
+        for (int i = 0; i < mMaxYGrid; i++) {
+            canvas.drawLine(75, mPosY + 75 + mSpacing * i, 75 + 1000, mPosY + 75 + mSpacing * i, paint);
+        }
+        Log.d(TAG, "onDraw x: " + mPosX + ", y: " + mPosY);
+
+//        paint.setStrokeWidth(0);
+//        paint.setTextAlign(Paint.Align.CENTER);
+//        paint.setTextSize(40);
+//        canvas.drawText("(" + mPosX + ", " + y, mPosX, y + 50, paint);
+    }
+
+    private float mPosX = 75, mPosY = 75;
+    private float mLastTouchX, mLastTouchY;
+    private int mActivePointerId = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "ACTION_DOWN");
+
+                int pointerIndex = event.getActionIndex();
+                float x = event.getX(pointerIndex);
+                float y = event.getY(pointerIndex);
+
+                // Remember where we started (for dragging)
+                mLastTouchX = x;
+                mLastTouchY = y;
+                // Save the ID of this pointer (for dragging)
+                mActivePointerId = event.getPointerId(0);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                Log.d(TAG, "ACTION_MOVE");
+
+                // Find the index of the active pointer and fetch its position
+                pointerIndex = event.findPointerIndex(mActivePointerId);
+
+                x = event.getX(pointerIndex);
+                y = event.getY(pointerIndex);
+
+                // Calculate the distance moved
+                float dx = x - mLastTouchX;
+                float dy = y - mLastTouchY;
+
+                if (isDragX) {
+                    mPosX += dx;
+                    if (mPosX >= 75) {
+                        mPosX = 75;
+                    }
+
+                    if (mPosX < -1875) {
+                        mPosX = -1875;
+                    }
+                }
+
+                if (isDragY)
+                    mPosY += dy;
+
+                invalidate();
+
+                // Remember this touch position for the next move event
+                mLastTouchX = x;
+                mLastTouchY = y;
+
+                break;
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/com/albertkhang/testcandlestickchart/MainActivity.java
+++ app/src/main/java/com/albertkhang/testcandlestickchart/MainActivity.java
@@ -1,14 +1,23 @@
 package com.albertkhang.testcandlestickchart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        Chart chart = new Chart(this);
+        setContentView(chart);
+        chart.invalidate();
     }
 }
